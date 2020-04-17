f3cafd66bc8129bb00c3abbfd12f36873323a422
Abraham Nieva de la Hidalga <a_nieva@hotmail.com>
2020-04-17 16:17:18 +0100

    python script reading html document

--- README.md
+++ README.md
@@ -4,13 +4,15 @@ Examples using Chem Data Extractor
 A demonstrator of basic use of Chem Data Extractor using jupyter notebooks 
 
 Jupyter Example Files:
-  - CDE Getting Started.ipynb   Basic example repeating the initial steps in the CDE documentation
-  - Read File pdf.ipynb         Example reading a single file
-  - Read CSV List               Example reading from a csv list, modified from the example of cde_read_files.py
+  1. CDE Getting Started.ipynb   Basic example repeating the initial steps in the CDE documentation
+  2. Read PDF File.ipynb         Example reading a single file
+  3. Read CSV List.ipynb         Example reading from a csv list, modified from the example of cde_read_files.py
+  4. Read HTML File.ipynb        Example reading a single HTML document from the web (creating a temporary local copy)
   
 Python Example Files: 
-  - cde_demo.py         script running the same comands as the two jupyter notebooks
-  - cde_read_files.py   script for reading several pdf files from a directory, runs from the command line:
-                        python cde_read_files.py ./pdfs
+  1. cde_demo.py         script running the same comands as the two jupyter notebooks
+  2. cde_read_files.py   script for reading several pdf files from a directory, runs from the command line:
+                         python cde_read_files.py ./pdfs
+  3. cde_read_html.py    script for reading an online HTML document (derived from Jupyter example 4)
                         
 The code does not include the example input pdf file, this can be downloaded from https://pubs.rsc.org/ko/content/articlehtml/2019/cp/c8cp05975f
---
+++ cde_read_html.py
@@ -0,0 +1,58 @@
+# import Document library from Chem Data Extractor
+from chemdataextractor import Document
+# import library for managing html requests
+import requests
+
+# The HTML document used in this example is the online version of:
+# Silveri, F., Quesne, M. G., Roldan, A., De Leeuw, N. H., & Catlow, C. R. A. (2019). 
+# Hydrogen adsorption on transition metal carbides: a DFT study. Physical Chemistry 
+# Chemical Physics, 21(10), 5335-5343.
+
+# the path for the html file is used directly
+article_url = "https://pubs.rsc.org/ko/content/articlehtml/2019/cp/c8cp05975f"
+
+# set request header
+req_head = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
+# get the page content 
+html_response = requests.get(article_url, headers = req_head)
+#save the content as a temporary file in the local disk
+f= open("temp.html","w+")
+f.write(str(html_response.content))
+f.close()
+
+# open de temporary file and read in binary mode
+f = open("temp.html", 'rb')
+
+# create a document object from the file
+doc = Document.from_file(f)
+
+
+for element in doc.elements:
+    print(element)
+
+para = doc.elements[14]
+
+print(para)
+print("Sentences:", len(para.sentences))
+print("Tokens:", para.tokens)
+print("Tokens:", len(para.tokens))
+print("Tokens:", len(para.tokens[0]))
+
+#list of unique occurences
+uniques=[]
+for chement in doc.cems:
+    #print(chement)
+    if not chement.text in uniques:
+        uniques.append(chement.text)
+
+print("Unique entities:",len(uniques))
+
+serialised=doc.records.serialize()
+
+print(len(serialised))
+
+print(serialised)
+
+##if __name__ == "__main__":
+##   cde_read_pdfs(sys.argv[1:])
+
